d9de4f3667fbd973c085577dd0c1711566a9d71e
sujal-st <sujal.sthapit4@gmail.com>
2026-03-16 23:32:28 +0545

    feat(responsive nabvar): commit

--- frontend/frontend/app/routes/home.tsx
+++ frontend/frontend/app/routes/home.tsx
@@ -1,13 +1,12 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Sujal Sthapit" },
+    { name: "description", content: "Portfolio" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <>Home</>;
 }
