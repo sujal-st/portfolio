9501b718412a6865ef8604445cb3744360a3e7a7
sujal-st <sujal.sthapit4@gmail.com>
2026-03-17 21:55:03 +0545

    fix(download-btn)

--- frontend/frontend/app/components/CtaButtons.tsx
+++ frontend/frontend/app/components/CtaButtons.tsx
@@ -9,16 +9,16 @@ function CtaButtons({type}:PropType) {
   return (
     <button className={`px-6 py-3 md:w-fit rounded-full font-semibold flex items-center justify-center gap-2 cursor-pointer hover:scale-105 transition ${type==='secondary'? 'bg-btn-seconadry border-2 border-gray-700 text-primary hover:bg-gray-700 hover:text-white':'bg-secondary text-white hover:bg-hover-btn-primary'}`}>
         {type==='secondary'?(
-            <>
+            <a href='/Sujal_Sthapit_Resume.pdf' download="Sujal_Sthapit_Resume.pdf">
             <DownloadIcon/>
             Download Resume
-            </>
+            </a>
         ):
         (
-            <>
+            <a href='#projects'>
             <RemoveRedEyeIcon/>
             View Projects
-            </>
+            </a>
         )}
     </button>
   )
