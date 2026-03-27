ea689926451c238368f25efdab617e781d454376
sujal-st <sujal.sthapit4@gmail.com>
2026-03-27 21:41:56 +0545

    project added

--- frontend/frontend/app/components/ProjectSection.tsx
+++ frontend/frontend/app/components/ProjectSection.tsx
@@ -6,7 +6,7 @@ function ProjectSection({projects}:{projects:project[]}) {
   return (
     <section id="projects" className='max-w-5xl mx-auto mt-30'>
       <h2 className='text-5xl text-primary text-center font-semibold mb-11'>Projects</h2>
-      <div className="px-8 md:px-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div className="px-8 mx-auto md:px-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-[repeat(auto-fit,350px)] justify-center gap-8">
         {projects.map((p:project) => (
           <ProjectCard key={p.id} p={p}/>
         ))}
